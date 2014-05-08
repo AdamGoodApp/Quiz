279ef47fb631ce2b0ac4f9134806da4303dc4993
Adam Rais <adamgoodapp@gmail.com>
2014-05-08 19:25:03 +0100

    Remove game and start using the @property method

--- MyFirstObjectiveCProgram/MyFirstObjectiveCProgram/main.m
+++ MyFirstObjectiveCProgram/MyFirstObjectiveCProgram/main.m
@@ -7,52 +7,10 @@
 //
 
 #import <Cocoa/Cocoa.h>
-#import "Animal.h"
 
 int main()
 {
-    Animal *dog = [[Animal alloc] init];
-    [dog setScore:0];
-//
-//    NSString *human = @"Adam Rais";
-//    
-//    NSLog(@"\n My dogs name is %@ and my name is %@ \n", [dog name], human);
     
-//    NSLog(@" \n\n Welcome to the GAME!!");
-//    
-//    NSLog(@" \n\n What is 2 * 2?");
-    
-    char word[50] = {0};
-    
-    scanf("%s", word);
-    NSLog(@"%s", word);
-    
-    int answerOne = 0;
-    scanf("%d", &answerOne);
-    
-    if(answerOne == 4) {
-        NSLog (@"Correct, One point added");
-        [dog setScore: + 1];
-    } else {
-        NSLog (@"Inccorect");
-        [dog setScore: - 1];
-    }
-    
-    
-    NSLog(@"\n What is 10 - 5  \n");
-    
-    int answerTwo = 0;
-    scanf("%d", &answerTwo);
-    
-    if(answerTwo == 5) {
-        NSLog(@"well done!");
-        [dog setScore: 2];
-    } else {
-        NSLog(@"unlucky");
-        [dog setScore: 0];
-    }
-    
-    NSLog(@"Your final score is: %d", [dog score]);
     
     
     
