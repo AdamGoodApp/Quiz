c08125f5a3d35fe77f7f0c4c2d2b7ab14a131946
Adam Rais <adamgoodapp@gmail.com>
2014-05-08 19:34:32 +0100

    Adds header file for class Contestant

---
+++ MyFirstObjectiveCProgram/MyFirstObjectiveCProgram/Contestant.h
@@ -0,0 +1,13 @@
+//
+//  Contestant.h
+//  MyFirstObjectiveCProgram
+//
+//  Created by Rais on 08/05/2014.
+//  Copyright (c) 2014 treehouse. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Contestant : NSObject
+
+@end
---
+++ MyFirstObjectiveCProgram/MyFirstObjectiveCProgram/Contestant.m
@@ -0,0 +1,13 @@
+//
+//  Contestant.m
+//  MyFirstObjectiveCProgram
+//
+//  Created by Rais on 08/05/2014.
+//  Copyright (c) 2014 treehouse. All rights reserved.
+//
+
+#import "Contestant.h"
+
+@implementation Contestant
+
+@end
